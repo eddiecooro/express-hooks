839bac3b0f3f34a217897e338a710f40d0a14bca
Eddie CooRo <1300100eddiecooro@gmail.com>
2019-08-04 03:41:28 +0430

    Config webpack

---
+++ .eslintrc.js
@@ -0,0 +1,4 @@
+module.exports = {
+  extends: ['eslint:recommended'],
+  parser: 'babel-eslint'
+};
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const WebpackShellPlugin = require('webpack-shell-plugin');
+const nodeExternals = require('webpack-node-externals');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+const config = {
+  entry: './src/index.js',
+  target: 'node',
+  devtool: '#sourcemap',
+  externals: [nodeExternals()],
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  plugins: [],
+  optimization: {
+    minimizer: []
+  }
+};
+
+if (process.env.NODE_ENV !== 'production') {
+  config.plugins.push(
+    new WebpackShellPlugin({
+      onBuildEnd: ['nodemon dist/index.js --watch build']
+    }),
+    new webpack.BannerPlugin('require("source-map-support").install();', {
+      raw: true,
+      entryOnly: false
+    })
+  );
+  config.optimization.minimizer.push(
+    new UglifyJsPlugin({ cache: true, parallel: true, sourceMap: false })
+  );
+} else {
+  config.module.rules.push({
+    enforce: 'pre',
+    test: /\.js$/,
+    exclude: /node_modules/,
+    loader: 'eslint-loader',
+    options: {
+      emitWarning: true,
+      failOnError: false,
+      failOnWarning: false
+    }
+  });
+  config.plugins.push(
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  );
+}
+
+export default config;

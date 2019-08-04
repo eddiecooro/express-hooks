f75f37b7938f51f4fcc818cc0a2f799754bdd127
Eddie CooRo <1300100eddiecooro@gmail.com>
2019-08-04 22:14:36 +0430

    Add support for useRes

--- src/Hooks.js
+++ src/Hooks.js
@@ -1,4 +1,11 @@
 import { resolveDispatcher } from './utils';
+
 export function useParam(paramName) {
   const dispatcher = resolveDispatcher();
 }
+
+export function useRes() {
+  const dispatcher = resolveDispatcher();
+  return dispatcher._res;
+}
+
--- src/__tests__/hooks.test.js
+++ src/__tests__/hooks.test.js
@@ -1,7 +1,18 @@
-import { useParam } from '../Hooks';
+import { useParam, useRes, useReq } from '../Hooks';
+import CurrentDispatcher from '../CurrentDispatcher';
 
 describe('hooks', () => {
   it('hook throws error when used without dispatcher', () => {
     expect(() => useParam()).toThrowErrorMatchingSnapshot();
   });
+
+  describe('useRes', () => {
+    it('Returns the response object correctly', () => {
+      CurrentDispatcher.current = {
+        _res: 'Res'
+      };
+      expect(useRes()).toMatch('Res');
+    });
+  });
+
 });

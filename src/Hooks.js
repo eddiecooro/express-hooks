a94b0bdaf8167864d05993d955bec5505df4f719
Eddie CooRo <1300100eddiecooro@gmail.com>
2019-08-07 00:31:52 +0430

    Complete hook generator for Req hooks (TODO: returning default value)

--- src/Hooks.js
+++ src/Hooks.js
@@ -1,45 +1,4 @@
-import { resolveDispatcher } from './CurrentDispatcher';
-
-export function useParam(paramName, defaultValue) {
-	const req = useReq();
-	return (req.params && req.params[paramName]) || defaultValue;
-}
-
-export function useRes() {
-	const dispatcher = resolveDispatcher();
-	return dispatcher._res;
-}
-
-export function useReq() {
-	const dispatcher = resolveDispatcher();
-	return dispatcher._req;
-}
-
-export function useBaseUrl() {
-	const req = useReq();
-	return req.baseUrl;
-}
-
-export function usePath() {
-	const req = useReq();
-	return req.path;
-}
-
-export function useMethod() {
-	const req = useReq();
-	return req.method;
-}
-
-export function useQuery(queryName, defaultValue) {
-	if (!queryName) throw new Error('queryName parameter is required');
-	const req = useReq();
-	return req.query[queryName] || defaultValue;
-}
-
-export function useHostName() {
-	const req = useReq();
-	return req.hostname;
-}
+import { useRes } from './BaseHooks';
 
 export function useSetCookie(cookieName, cookieValue, options) {
 	const res = useRes();
@@ -56,32 +15,10 @@ export function useAttachment(fileName) {
 	return res.attachment(fileName);
 }
 
-export function useHeader(headerName, defaultValue) {
-	const req = useReq();
-	return req.get(headerName) || defaultValue;
-}
-
 export function useResponseHeader(headerName, defaultValue) {
 	const res = useRes();
 	return res.get(headerName) || defaultValue;
 }
 
-export function useIsAcceptable(contentType) {
-	const req = useReq();
-	return req.accepts(contentType);
-}
-
-export function useIsCharsetAcceptable(charSet) {
-	const req = useReq();
-	return req.acceptsCharsets(charSet);
-}
-
-export function useIsEncodingAcceptable(encoding) {
-	const req = useReq();
-	return req.acceptsEncodings(encoding);
-}
-
-export function useIsLanguageAcceptable(language) {
-	const req = useReq();
-	return req.acceptsLanguages(language);
-}
+export * from './BaseHooks';
+export * from './ReqHooks';
